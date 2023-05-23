0dbd419522947b3feb0aa8d61df2e9f354fc6f56
gabrielj <gabriel.ggcj@gmail.com>
2023-05-23 01:31:11 -0300

    feature/create-noTodoComponent

--- src/components/TodoListComponent/TodoListComponent.jsx
+++ src/components/TodoListComponent/TodoListComponent.jsx
@@ -1,32 +1,43 @@
+import { NoTodoComponent } from "../NoTodoComponent/NoTodoComponent"
 import styles from "./TodoListComponent.module.css"
 
 export const TodoListComponent = () => {
 
     return (
-        <div className={styles.todoList}>
 
-            <div className={styles.todoInformation}>
+        <div className={styles.todoListContainer}>
 
-                <div className={styles.textInformation}>
-                    <strong>
-                        <p className={styles.text1}>Tarefas Criadas</p>
-                    </strong>  
-                    <div className={styles.numberCreatedTodo}>
-                        <p>10</p>                  
+            <div className={styles.todoList}>
+
+                <div className={styles.todoInformation}>
+
+                    <div className={styles.textInformation}>
+
+                        <strong>
+                            <p className={styles.text1}>Tarefas Criadas</p>
+                        </strong>
+
+                        <div className={styles.numberCreatedTodo}>
+                            <p>10</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.textInformation}>
-                    <strong>
-                        <p className={styles.text2}>Tarefas Concluidas</p>                        
-                    </strong>
-                    <div className={styles.numberConcludedTodo}>
-                        <p>3 de 10</p>
+                    <div className={styles.textInformation}>
+
+                        <strong>
+                            <p className={styles.text2}>Tarefas Concluidas</p>
+                        </strong>
+
+                        <div className={styles.numberConcludedTodo}>
+                            <p>3 de 10</p>
+                        </div>
                     </div>
+
                 </div>
 
             </div>
 
+            <NoTodoComponent />
         </div>
     )
 }

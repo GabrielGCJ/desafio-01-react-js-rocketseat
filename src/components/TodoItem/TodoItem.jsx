86cfa0f29f40fdfee8397b39cf5d13683b3ce2ad
gabrielj <gabriel.ggcj@gmail.com>
2023-05-23 18:26:09 -0300

    feature/update-button-conclued

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,31 +1,36 @@
 import { useState } from "react";
 import styles from "./TodoItem.module.css"
 import { Circle, CheckCircle, Trash } from "phosphor-react";
-
+import concludedTrue from "../../assets/concludedTrue.svg"
+import concludedFalse from "../../assets/concludedFalse.svg"
 
 
 export const TodoItem = () => {
 
-    const [ concluded, setConcluded ] = useState("false")
+    const [concluded, setConcluded] = useState("false")
 
     const editConcluded = () => {
         if (concluded === "false") {
             setConcluded("true")
-        } else {
-            setConcluded("false")
-        }
+        } 
+        else { setConcluded("false")  }
     }
 
     return (
         <div className={styles.containerTodoItem}>
 
             <div className={styles.todoItem}>
-                <button concluded={concluded} className={styles.circle} onClick={editConcluded}>
+                <button
+                    concluded={concluded}
+                    className={styles.circle}
+                    onClick={editConcluded} >
 
-                    {concluded === "true" ? (<CheckCircle size={20} color="#8284FA" weight="fill" />) : (<Circle size={20} />)}
+                    {concluded === "true" ? (<img src={concludedTrue} alt="concludedTrue" />) : (<img src={concludedFalse} alt="concludedFalse" />)}
 
                 </button>
+
                 <p>Em meio às adversidades da vida, lembre-se sempre de que cada obstáculo é uma oportunidade disfarçada de crescimento. Você é capaz de superar qualquer desafio que surgir em seu caminho, desde que mantenha a chama da determinação acesa em seu coração</p>
+
                 <button className={styles.trash}><Trash size={20} /></button>
 
             </div>

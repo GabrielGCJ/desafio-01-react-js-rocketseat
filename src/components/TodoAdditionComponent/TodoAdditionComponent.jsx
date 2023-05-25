5a6f4d2363130f2443e0dfde4e4c05e2464bdbcf
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-05-25 01:49:16 -0300

    feature/update-props-toDo-appjs

--- src/components/TodoAdditionComponent/TodoAdditionComponent.jsx
+++ src/components/TodoAdditionComponent/TodoAdditionComponent.jsx
@@ -1,16 +1,90 @@
+// import { useState } from "react"
+// import styles from "./TodoAdditionComponent.module.css"
+
+// export const TodoAdditionComponent = ({handleCreateNewToDo}) => {
+
+//     // const [newCommentText, setNewCommentText] = useState("")
+
+//     // const handleNewCommentChange = () => {
+//     //     event.target.setCustomValidity("")
+
+//     //     setNewCommentText(event.target.value)
+//     // }
+// const [newTododo, setNewTododo] = useState("")
+
+// const text123 = () => {
+//     console.log(event.target.newToDoText)
+// }
+
+//     return(
+//         <form className={styles.toDoForm}>
+//             <textarea 
+//                 name="add-todo" 
+//                 placeholder="Adiciona uma nova tarefa" 
+//                 value={newToDoText}
+//                 required={true}
+//                 onChange={text123}
+
+//             />
+
+//             <footer>
+//                 <button
+//                     type="submit"
+//                 >
+//                     Criar
+//                 </button>
+//             </footer>
+//         </form>
+//     )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
 import styles from "./TodoAdditionComponent.module.css"
 
-export const TodoAdditionComponent = () => {
+export const TodoAdditionComponent = ({ handleCreateNewToDo }) => {
+    
+    const [newToDoText, setNewToDoText] = useState("");
 
-    return(
-        <form className={styles.toDoForm}>
-            <textarea 
-                name="add-todo" 
-                placeholder="Adiciona uma nova tarefa"  
+    const handleNewToDoChange = (event) => {
+
+        setNewToDoText(event.target.value);
+
+        
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(newToDoText)
+
+        setNewToDoText("")
+    }
+
+    return (
+        <form className={styles.toDoForm} 
+        onSubmit={handleCreateNewToDo}>
+            <textarea
+                name="addtodo"
+                placeholder="Adicione uma nova tarefa"
+                value={newToDoText}
+                required={true}
+                onChange={handleNewToDoChange}
             />
 
             <footer>
-                <button>
+                <button type="submit">
                     Criar
                 </button>
             </footer>
